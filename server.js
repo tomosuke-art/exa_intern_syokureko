6fcbf790dab5799d02632f480d20559c367ef264
Glitch (hello-express) <none>
2020-09-24 11:40:01 +0000

    🎓🛫 Checkpoint ./views/failed.html:582160/1037 ./server.js:582160/1653 ./package.json:582160/1454

--- server.js
+++ server.js
@@ -52,6 +52,11 @@ app.post('/signup', function(req, res){
     const db = client.db(process.env.DB); // 対象 DB
     const col = db.collection('accounts'); // 対象コレクション
     const user = {name: userName, password:password}; // 保存対象
+    // ★★★本来パスワードは平文（入力されたそのままの文字列）で保存すべきではない
+    // crypto モジュールでハッシュ化するなどすべき
+    // ログインの際も入力されたパスワードをハッシュ化した上で
+    // 保存されているハッシュ化済みのパスワードと比較する
+    // 参考：https://qiita.com/kou_pg_0131/items/174aefd8f894fea4d11a
     col.insertOne(user, function(err, result) {
       res.redirect('/'); // リダイレクト
       client.close(); // DB を閉じる
@@ -62,10 +67,14 @@ app.post('/signup', function(req, res){
 app.post('/login', function(req, res){
   const userName = req.body.userName;
   const password = req.body.password;
+  console.log(hashed(password));
   MongoClient.connect(mongouri, function(error, client) {
     const db = client.db(process.env.DB); // 対象 DB
     const col = db.collection('accounts'); // 対象コレクション
 
+    // 登録時にパスワードをハッシュ化しているならば
+    // ここで password をハッシュ化して検索する
+    // ハッシュ化した値同士で比較する
     const condition = {name:{$eq:userName}, password:{$eq:password}}; // ユーザ名とパスワードで検索する
     col.findOne(condition, function(err, user){
       client.close();
@@ -78,5 +87,15 @@ app.post('/login', function(req, res){
     });
   });
 });
+
+// ハッシュ化用
+const crypto = require('crypto');
+
+function hashed(password) {
+  let hash = crypto.createHmac('sha512', password)
+  hash.update(password)
+  let value = hash.digest('hex')
+  return value;
+}
 
 const listener = app.listen(process.env.PORT);
